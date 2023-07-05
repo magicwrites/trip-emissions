3f84e784b1f3c2059ca438e7192e5eb5567d29e6
Maciej Mazur <maciej@mazur.site>
2023-07-05 14:16:59 +0200

    underscore for unused variable

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,7 @@ import { rest } from "msw";
 import trips from "./trips.json";
 
 export const handlers = [
-  rest.get("/api/trips", (req, res, ctx) => {
+  rest.get("/api/trips", (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(trips));
   }),
 ];
