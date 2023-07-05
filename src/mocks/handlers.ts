793a27ce22ec886af942d3116b027fe14abc23f9
Maciej Mazur <maciej@mazur.site>
2023-07-05 14:13:04 +0200

    moved data to a separate json file

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,31 +1,5 @@
 import { rest } from "msw";
-
-const trips = [
-  {
-    name: "European quest",
-    image: "https://images.unsplash.com/photo-1511192806308-9b77cd6f754c",
-    countries: 8,
-    days: 21,
-    emissions: 810,
-    rating: 4.7,
-  },
-  {
-    name: "European quest",
-    image: "https://images.unsplash.com/photo-1511192806308-9b77cd6f754c",
-    countries: 8,
-    days: 21,
-    emissions: 3.56,
-    rating: 4.7,
-  },
-  {
-    name: "European quest",
-    image: "https://images.unsplash.com/photo-1511192806308-9b77cd6f754c",
-    countries: 8,
-    days: 21,
-    emissions: 6.92,
-    rating: 4.7,
-  },
-];
+import trips from "./trips.json";
 
 export const handlers = [
   rest.get("/api/trips", (req, res, ctx) => {
