2810b5463a12d246b6a3ec7c539871ed475752e8
Maciej Mazur <maciej@mazur.site>
2023-07-05 14:20:38 +0200

    remove test and bypass unjustified warnings

--- src/main.tsx
+++ src/main.tsx
@@ -7,24 +7,10 @@ import "./index.css";
 // running mock worker in production is discouraged in the documentation,
 // but this is exactly what we need for our exercise here so we roll with it
 
-worker.start();
+worker.start({ onUnhandledRequest: "bypass" });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-const testWorker = async () => {
-  const response = await fetch("/api/trips");
-
-  if (response.ok) {
-    const trips = await response.json();
-
-    console.info({ trips });
-  } else {
-    console.warn("could not retrieve trips");
-  }
-};
-
-testWorker();
